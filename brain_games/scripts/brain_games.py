e215ca180033cc1adc5aecc1da987d8486f54e68
zluuba <zluyba.nikitina@gmail.com>
2022-11-12 13:55:14 +0300

    Fix: correct linter errors in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def greet():
-        print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+
 
 def main():
-	greet()
-	welcome_user()
+    greet()
+    welcome_user()
+
 
 if __name__ == '__main__':
-	main()
+    main()
