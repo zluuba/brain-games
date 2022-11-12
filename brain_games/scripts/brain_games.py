0e49f6aa2093bc8ca0d503acfd16c4db832d3e50
zluuba <zluyba.nikitina@gmail.com>
2022-11-12 12:53:37 +0300

    Fix: add welcome_user to brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+def greet():
+        print('Welcome to the Brain Games!')
 
 def main():
-    print('Welcome to the Brain Games!')
+	greet()
+	welcome_user()
 
 if __name__ == '__main__':
-    main()
+	main()
