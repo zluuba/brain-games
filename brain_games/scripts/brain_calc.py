3cf398027b2e36cd4c6ccdb1f28c4fd1873ab0f3
zluuba <zluyba.nikitina@gmail.com>
2022-11-13 13:54:55 +0300

    Feat: add brain_calc script

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,15 @@
+from brain_games.games.calc_game import calculator, welcome
+
+
+def calc():
+    calculator()
+
+
+def main():
+    welcome()
+    calc()
+
+
+if __name__ == '__main__':
+    main()
+
