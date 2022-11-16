43e10d7757d154e6afbddccb6f83e5bb2c5ca8c5
zluuba <zluyba.nikitina@gmail.com>
2022-11-16 13:50:46 +0300

    Fix: try to fix duplication in scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 from brain_games.games.calc_game import calc_welcome, calculator
 
 
-def main():
+def main_calc():
     calc_welcome()
     calculator()
 
 
 if __name__ == '__main__':
-    main()
+    main_calc()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 from brain_games.games.even_game import even_welcome, is_even_game
 
 
-def main():
+def main_even():
     even_welcome()
     is_even_game()
 
 
 if __name__ == '__main__':
-    main()
+    main_even()
