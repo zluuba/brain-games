8625ceab9071bb33efd49627d339e0c8866c34db
zluuba <zluyba.nikitina@gmail.com>
2022-11-19 13:55:17 +0300

    Fix: minor changes

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,9 +2,9 @@ from brain_games.games.calc_game import calculator
 from brain_games.games_body import start_game
 
 
-def calc_game():
+def game_calculator():
     start_game(calculator)
 
 
 if __name__ == '__main__':
-    calc_game()
+    game_calculator()
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,9 +2,9 @@ from brain_games.games.progression_game import progression
 from brain_games.games_body import start_game
 
 
-def progression_game():
+def game_progression():
     start_game(progression)
 
 
 if __name__ == '__main__':
-    progression_game()
+    game_progression()
