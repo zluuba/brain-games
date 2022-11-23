7dddac952e0835f990e26369016ecbf635fe77df
zluuba <zluyba.nikitina@gmail.com>
2022-11-23 14:05:24 +0300

    Fix: minor changes

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -4,17 +4,23 @@ from random import randint
 game_rule = 'Find the greatest common divisor of given numbers.'
 
 
+# create random numbers
 def get_numbers():
     num1 = randint(1, 25)
     num2 = randint(1, 25)
     return num1, num2
 
 
-# finds the Greatest Common Divisor (gcd)
-def get_gcd_result():
+# create an expression that the user will see
+def get_expression():
     num1, num2 = get_numbers()
-    # numbers - what user see
     numbers = f'{num1} {num2}'
+    return num1, num2, numbers
+
+
+# find result - the Greatest Common Divisor(gcd)
+def get_gcd_result():
+    num1, num2, numbers = get_expression()
     while num1 != 0 and num2 != 0:
         if num1 > num2:
             num1 = num1 % num2
