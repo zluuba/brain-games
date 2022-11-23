1443ecf08620f4b6a3630e3574ea8a792bd9a449
zluuba <zluyba.nikitina@gmail.com>
2022-11-23 13:40:03 +0300

    Fix: rebuild functions

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,30 +1,29 @@
 import random
-# import operator
+from operator import add, sub, mul
 from random import randint
 
 
 game_rule = 'What is the result of the expression?'
 
 
-def get_expression_parts():
-    num1 = randint(1, 25)
-    num2 = randint(1, 25)
-    operators = ['+', '-', '*']
-    operator = random.choice(operators)
-    return num1, num2, operator
+# create random numbers and get random operator from dictionary
+# count the result and pass all of this to get_expression
+def get_expression_parts_and_result():
+    num1, num2 = randint(1, 25), randint(1, 25)
+    operators_dictionary = {
+        "+": add(num1, num2),
+        "-": sub(num1, num2),
+        "*": mul(num1, num2)
+    }
+    operator_ = random.choice(list(operators_dictionary.keys()))
+    result = operators_dictionary[operator_]
+    return num1, num2, operator_, result
 
 
 # create one simple math expression, calculate the result
-# example: '14 + 9', '23 - 7', '5 * 12'
-def get_expression_and_result():
-    num1, num2, operator = get_expression_parts()
-    expression = f'{num1} {operator} {num2}'
-    if operator == "+":
-        result = num1 + num2    # add(a, b)
-    elif operator == "-":
-        result = num1 - num2    # sub(a, b)
-    else:
-        result = num1 * num2    # mul(a, b)
+def get_expression():
+    num1, num2, operator_, result = get_expression_parts_and_result()
+    expression = f'{num1} {operator_} {num2}'
     return result, expression
 
 
@@ -33,7 +32,7 @@ def get_calc_lists():
     questions_calc_list = []
     results_calc_list = []
     for _ in range(3):
-        result, expression = get_expression_and_result()
+        result, expression = get_expression()
         questions_calc_list.append(expression)
         results_calc_list.append(str(result))
     return questions_calc_list, results_calc_list
