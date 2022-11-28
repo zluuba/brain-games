c33d99f80867532d9ba72de480cbb99e6cf4aaf2
zluuba <zluyba.nikitina@gmail.com>
2022-11-28 16:07:44 +0300

    Fix: change var names

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -5,20 +5,15 @@ game_rule = 'What number is missing in the progression?'
 
 
 def get_result_and_expression():
-    first_num = randint(1, 20)
     step = randint(2, 6)
-    list_volume = randint(5, 11)
-
-    # create hide char index and last number of progression for range function
-    hide_char_index = randint(0, list_volume - 1)
-    last_num = first_num + (list_volume * step)
+    progression_lenght = randint(5, 11)
+    hide_char_index = randint(0, progression_lenght - 1)
+    first_num = randint(1, 20)
+    last_num = first_num + (progression_lenght * step)
 
     # create final progression and hide char
-    final_progression = list(range(first_num, last_num, step))
-    hide_digit = str(final_progression[hide_char_index])
-    final_progression[hide_char_index] = '..'
-
-    # create progression what user see (with hide char)
-    progression_expression = " ".join(map(str, final_progression))
-
-    return hide_digit, progression_expression
+    progression = list(range(first_num, last_num, step))
+    hide_digit = str(progression[hide_char_index])
+    progression[hide_char_index] = '..'
+    expression = " ".join(map(str, progression))
+    return hide_digit, expression
