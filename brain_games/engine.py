909f55aa0dd4cf26e5285ae5c2a4a6b95806724c
zluuba <zluyba.nikitina@gmail.com>
2022-11-30 10:34:47 +0300

    Refactor: combine functions, rename vars

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,47 +1,34 @@
 import prompt
 
 
+ROUNDS = 3
+
+
 def get_user_name_and_show_welcome(game_rule):
     print('Welcome to the Brain Games!')
-    name = prompt.string("May I have your name? ")
-    print(f'Hello, {name}!')
+    user_name = prompt.string("May I have your name? ")
+    print(f'Hello, {user_name}!')
     print(game_rule)
-    return name
+    return user_name
 
 
-def get_user_answer_and_show_question(expression):
-    print(f'Question: {expression}')
+def get_user_answer_and_show_question(question):
+    print(f'Question: {question}')
     answer = prompt.string('Your answer: ')
     return answer
 
 
-def is_correct_check_and_show(answer, result):
-    if answer == result:
-        print('Correct!')
-        return True
-    else:
-        print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
-        return False
-
-
-def show_user_win(name):
-    print(f'Congratulations, {name}!')
-
-
-def show_user_lose(name):
-    print(f"Let's try again, {name}!")
-
-
 def start_game(game):
     game_rule = game.game_rule
-    name = get_user_name_and_show_welcome(game_rule)
-    rounds = 3
-    for _ in range(rounds):
-        result, expression = game.get_result_and_expression()
-        answer = get_user_answer_and_show_question(expression)
-        correct = is_correct_check_and_show(answer, str(result))
-        if not correct:
-            show_user_lose(name)
+    user_name = get_user_name_and_show_welcome(game_rule)
+    for _ in range(ROUNDS):
+        correct_answer, question = game.get_result_and_question()
+        user_answer = get_user_answer_and_show_question(question)
+        if user_answer != str(correct_answer):
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {user_name}!")
             break
+        print('Correct!')
     else:
-        show_user_win(name)
+        print(f'Congratulations, {user_name}!')
