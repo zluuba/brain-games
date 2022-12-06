f2a815e1fbab83ce92d881f99f3e1897e730644b
zluuba <zluyba.nikitina@gmail.com>
2022-12-06 08:08:57 +0300

    Fix: minor changes, del dividor var

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -6,15 +6,12 @@ game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def get_question_and_answer():
     number = randint(2, 100)
-    divider = 2
-    bool_answer = is_prime(number, divider)
-    right_answer = 'yes' if bool_answer else 'no'
+    right_answer = 'yes' if is_prime(number) else 'no'
     return number, right_answer
 
 
-def is_prime(number, divider):
-    while divider <= number / 2:
+def is_prime(number):
+    for divider in range(2, (number // 2) + 1):
         if number % divider == 0:
             return False
-        divider += 1
     return True
