d54b12dd719b3316fdb6257914bbd4c65ff2d9a8
zluuba <zluyba.nikitina@gmail.com>
2022-12-06 08:16:07 +0300

    Fix: minor changes

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -6,7 +6,8 @@ game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def get_question_and_answer():
     number = randint(2, 100)
-    right_answer = 'yes' if is_prime(number) else 'no'
+    number_is_prime = is_prime(number)
+    right_answer = 'yes' if number_is_prime else 'no'
     return number, right_answer
 
 
